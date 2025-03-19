6200fbcba3932810aa133756e96c49d27b6a4ddc
Gitixig <hatuandat190290@gmail.com>
2025-03-19 23:09:54 +0700

    tao qua luoi de lam tiep

--- hahahaha.php
+++ hahahaha.php
@@ -5,11 +5,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>hahahahahhahaha</title>
 </head>
 <body>
-        <p>asdasdasdasd</p>
-    ewgfuywegfgewyfrg
+    <table></table>
+        <div class="container">
+            <div class="product-box">
+                <img src="image/p258020801.png">
+                <div class="product-title">Cristiano Ronaldo</div>
+                <div class="product-price">1 tỷ đô</div>
+            </div>
+        </div>
 </body>
 <?php include 'layout/footer.php';
 ?>
