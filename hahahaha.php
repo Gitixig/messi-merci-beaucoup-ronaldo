f9432118781fb2828862c74749ef048bfb29f695
Gitixig <hatuandat190290@gmail.com>
2025-03-20 12:39:15 +0700

    hahahhahahahha

--- hahahaha.php
+++ hahahaha.php
@@ -6,15 +6,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hahahahahhahaha</title>
+    <style>
+        .product-box{
+            text-align: center;
+            border-radius: 10px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+        .product-box:hover{
+            transition: ease-out;
+        }
+    </style>
 </head>
 <body>
-    <table></table>
         <div class="container">
+            <div class="row g-3">
+                <div class="col-6 col-md-4 col-lg-3">
+            <div class="product-box">
+                <img src="image/p258020801.png">
+            </div>
+                <div class="product-infor">
+                <div class="product-title">Cristiano Ronaldo</div>
+                <div class="product-price">1 tỷ đô</div>
+            </div>
+            
+            </div>
+            <div class="col-6 col-md-4 col-lg-3">
             <div class="product-box">
                 <img src="image/p258020801.png">
                 <div class="product-title">Cristiano Ronaldo</div>
                 <div class="product-price">1 tỷ đô</div>
             </div>
+        </div>
+        <div class="col-6 col-md-4 col-lg-3">
+            <div class="product-box">
+                <img src="image/p258020801.png">
+                <div class="product-title">Cristiano Ronaldo</div>
+                <div class="product-price">1 tỷ đô</div>
+            </div>
+        </div>
+        <div class="col-6 col-md-4 col-lg-3">
+            <div class="product-box">
+                <img src="image/p258020801.png">
+                <div class="product-title">Cristiano Ronaldo</div>
+                <div class="product-price">1 tỷ đô</div>
+            </div>
+            </div>
+        </div>
         </div>
 </body>
 <?php include 'layout/footer.php';
