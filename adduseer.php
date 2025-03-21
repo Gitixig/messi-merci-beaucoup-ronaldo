3263ac4728750f0fd3b6606ca83384f85c3e434d
nhat21052006 <90zuka09@gmail.com>
2025-03-22 00:15:11 +0700

    nhatdeptrai

--- adduseer.php
+++ adduseer.php
@@ -1,27 +1,38 @@
 <?php include 'layout/header.php' ?>
-<style>
-    .information{
-        text-align: center;
-    }
-    .information ul{
-        text-align: center;
-        padding-right: 50px;
-    }
-    .submit{
-        text-align: center;
-    }
-</style>
-<form action="adduser_post.php" method="post">
-    <div class="information">
-    <ul>Name</ul>
-    <input type="text" name="name" placeholder="Name">
-    <ul>Username</ul>
-    <input type="text" name="username" placeholder="Username">
-    <ul>Password</ul>
-    <input type="password" name="password" placeholder="password">
+<div class="container mt-4">
+    <div class="card">
+        <div class="card-header bg-info text-white">
+            <h4 class="mb-0">Login</h4>
+        </div>
+        <div class="card-body">
+            <style>
+                .information {
+                    text-align: center;
+                }
+
+                .information ul {
+                    text-align: center;
+                    padding-right: 50px;
+                }
+
+                .submit {
+                    text-align: center;
+                }
+            </style>
+            <form action="adduser_post.php" method="post">
+                <div class="information">
+                    <ul>Name</ul>
+                    <input type="text" name="name" placeholder="Name">
+                    <ul>Username</ul>
+                    <input type="text" name="username" placeholder="Username">
+                    <ul>Password</ul>
+                    <input type="password" name="password" placeholder="password">
+                </div>
+                <div class="submit">
+                    <button type="submit">Đăng kí</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-<div class="submit">
-    <button type="submit">Đăng kí</button>
-</div>
-</form>
 <?php include 'layout/footer.php' ?>
--- layout/header.php
+++ layout/header.php
@@ -4,40 +4,43 @@
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Title site</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="hahahaha.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-                <?php 
-                if (!isset($_SESSION['UserLogin'])) {
-                    
-                    echo '<li class="nav-item"><a class="nav-link" href="adduseer.php">Sign in</a></li>';
-                    echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
-                } else {
-                    
-                    echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
-                }
-                if (isset($_SESSION['Role']) && $_SESSION['Role'] === "admin") {
-                        
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                    <?php
+                    if (!isset($_SESSION['UserLogin'])) {
+
+                        echo '<li class="nav-item"><a class="nav-link" href="adduseer.php">Sign in</a></li>';
+                        echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
+                    } else {
+
+                        echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
+                    }
+                    if (isset($_SESSION['Role']) && $_SESSION['Role'] === "admin") {
+
                         echo '<li class="nav-item"><a class="nav-link text-warning" href="addplayer.php">Add Player</a></li>';
                     }
-                ?>
-                  
-                </ul>
-        </div>
-    </div>
-</nav>
+                    ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
